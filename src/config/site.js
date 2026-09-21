82b6a25ae7f148a4b84d9cd853f0e52b20969c85
Kunal Singh <madebykunal@gmail.com>
2026-09-21 20:02:56 +0530

    Major Revamp

---
+++ src/config/site.js
@@ -0,0 +1,11 @@
+/**
+ * Global site configuration and metadata
+ */
+export const siteConfig = {
+  name: 'PlayZ',
+  title: 'PlayZ - Retro Arcade Games',
+  description: 'A retro arcade games collection platform.',
+  links: {
+    github: 'https://github.com/madebykunal/PlayZ'
+  }
+};
